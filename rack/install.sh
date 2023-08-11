adddecd56dc0011b5866f439a0b7e590e9850d11
Artem Timofeev <Legalaze94@gmail.com>
2023-08-12 01:16:44 +0400

    link rack repos

--- rack/install.sh
+++ rack/install.sh
@@ -5,3 +5,7 @@ TMP_DIR=/tmp/
 wget -q -P $TMP_DIR $URL
 unzip -q $TMP_DIR$FILE_NAME -d $HOME/
 rm $TMP_DIR$FILE_NAME
+
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+git clone git@github.com:atimofeev/vcv-rack2-settings.git $CUR_DIR/dotfiles
+git clone git@github.com:atimofeev/vcv-rack2-patches.git $HOME/repos/vcv-rack2-patches/
