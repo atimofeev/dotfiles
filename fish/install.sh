7b17dbae1ca64dd002fcfb38ee4c88046c91c85d
Artem Timofeev <Legalaze94@gmail.com>
2023-08-11 18:31:08 +0400

    FS refactored: reqs, install.sh, symlinks

--- fish/install.sh
+++ fish/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo dnf install -y fish #fzf
+sudo dnf install -yq fish #fzf
 
 
 ### FUZZY CD ###
 # https://brettterpstra.com/2021/12/24/a-fuzzy-cd-command-for-fish/
-# Fuzzy CD requirements: fasd, jump
+## FASD ##
 # https://github.com/clvv/fasd
 #curl -L https://github.com/clvv/fasd/archive/refs/tags/1.0.1.tar.gz
 #PREFIX=$HOME make install 
@@ -12,12 +12,9 @@ sudo dnf install -y fish #fzf
 # Put in shell rc
 #eval "$(fasd --init auto)"
 
+## JUMP ##
 # https://github.com/oh-my-fish/plugin-jump
 # probably can be installed by manual cp, or via adding adding or listing marks
 # https://jeroenjanssens.com/navigate/
 #...
 #fisher install ttscoff/fuzzy_cd
-
-### EXA ###
-# Just add this to config:
-# https://github.com/ogham/exa/blob/master/completions/fish/exa.fish
