701bd605dfe4659e21787a86c6a721b62a101b8f
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-08-14 05:44:57 +0400

    + abbr: err to dev/null 'dn'

--- fish/dotfiles/config.fish
+++ fish/dotfiles/config.fish
@@ -8,6 +8,9 @@ alias a=ansible
 alias k=kubectl
 alias rack='cd ~/Rack2Free && ./Rack && cd -'
 
+# abbreviations:
+abbr -a dn '2>/dev/null'
+
 # env variables:
 set GOPATH "$HOME/go"
 set PATH "$GOPATH/bin:$PATH"
