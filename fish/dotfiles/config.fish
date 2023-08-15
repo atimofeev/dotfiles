564bb56bb74fc937a1cca3a9a3699e2f35bfdcf0
Artem Timofeev <Legalaze94@gmail.com>
2023-08-16 00:17:38 +0400

    + archive extraction; config refactor

--- fish/dotfiles/config.fish
+++ fish/dotfiles/config.fish
@@ -8,28 +8,17 @@ set PATH "$GOPATH/bin:$PATH"
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"   # man pages -> bat
 set -x MANROFFOPT "-c"                              # bat man pages formatting fix
 
-### ALIASES ###
-alias t=terraform
-alias a=ansible
-alias k=kubectl
-alias rack='cd ~/Rack2Free && ./Rack && cd -'
 
-# ls -> exa
+### APPS ###
+
+# EXA #
 alias ls='exa --color=always --group-directories-first'
 alias ll='exa -l --color=always --group-directories-first'
 alias la='exa -al --color=always --group-directories-first'
 alias lt='exa -aT --color=always --group-directories-first --level 2' # tree listing
 alias l.='exa -a | egrep "^\."' # show only dotfiles
 
-alias mv='git mv $argv; or mv $argv'
-alias cd.='cd ..'
-alias cd..='cd ../..'
-alias cd...='cd ../../..'
-alias cd....='cd ../../../..'
-alias cd.....='cd ../../../../..'
-alias cd......='cd ../../../../../..'
-
-# bat
+# BAT #
 alias less='bat'
 alias cat='bat --paging=never'
 function help # help [command] -> bat
@@ -42,28 +31,19 @@ function fdb
     fd $argv -X bat
 end
 
-# ripgrep
+# RIPGREP #
 alias rg='rg -i --color=always'
 
-# fzf
+# FZF #
 alias fzps="ps -ef | fzf --bind 'ctrl-r:reload(ps -ef)' \
     --header 'Press CTRL-R to reload' --header-lines=1 \
     --height=70%--layout=reverse"
 alias fzfb='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
 function fzrg
     rg --line-number --no-heading --color=always --smart-case $argv | fzf -d ":" -n 2.. --ansi --no-sort --preview-window "down:20%:+{2}" --preview "bat --style=numbers --color=always --highlight-line {2} {1}"
-end  
+end
 
-# nvim
-alias vi='nvim'
-alias vim='nvim'
-
-# [command] | tb
-alias tb="nc termbin.com 9999"
-
-alias chx='chmod +x'
-
-# git
+# GIT #
 alias addup='git add -u'
 alias addall='git add .'
 alias branch='git branch'
@@ -75,6 +55,70 @@ alias pull='git pull origin'
 alias push='git push origin'
 alias tag='git tag'
 alias newtag='git tag -a'
+
+# ARCHIVE EXTRACTION #
+function ex
+    if test -f "$argv[1]"
+        switch (string match -r '\.\w+$' "$argv[1]")
+            case '*.tar.bz2'
+                tar xjf $argv[1]
+            case '*.tar.gz'
+                tar xzf $argv[1]
+            case '*.bz2'
+                bunzip2 $argv[1]
+            case '*.rar'
+                unrar x $argv[1]
+            case '*.gz'
+                gunzip $argv[1]
+            case '*.tar'
+                tar xf $argv[1]
+            case '*.tbz2'
+                tar xjf $argv[1]
+            case '*.tgz'
+                tar xzf $argv[1]
+            case '*.zip'
+                unzip $argv[1]
+            case '*.Z'
+                uncompress $argv[1]
+            case '*.7z'
+                7z x $argv[1]
+            case '*.deb'
+                ar x $argv[1]
+            case '*.tar.xz'
+                tar xf $argv[1]
+            case '*.tar.zst'
+                unzstd $argv[1]
+            case '*'
+                echo "'$argv[1]' cannot be extracted via ex()"
+        end
+    else
+        echo "'$argv[1]' is not a valid file"
+    end
+end
+
+
+### ALIASES ###
+alias t=terraform
+alias a=ansible
+alias k=kubectl
+alias rack='cd ~/Rack2Free && ./Rack && cd -'
+
+alias mv='git mv $argv; or mv $argv'
+alias cd.='cd ..'
+alias cd..='cd ../..'
+alias cd...='cd ../../..'
+alias cd....='cd ../../../..'
+alias cd.....='cd ../../../../..'
+alias cd......='cd ../../../../../..'
+
+# nvim
+alias vi='nvim'
+alias vim='nvim'
+
+# [command] | tb
+alias tb="nc termbin.com 9999"
+
+alias chx='chmod +x'
 
 # adding flags
 alias df='df -h'
