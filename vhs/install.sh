e384c548e5f11aaaf09e9b526b5bd3baa24d89a0
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-08-29 02:31:37 +0400

    vhs: variables for installation url and path

--- vhs/install.sh
+++ vhs/install.sh
@@ -1,11 +1,14 @@
 # Fedora/RHEL only
+
+TTYD="/usr/local/bin/ttyd"
+TTYD_URL="https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64"
+
 echo '[charm]
 name=Charm
 baseurl=https://repo.charm.sh/yum/
 enabled=1
 gpgcheck=1
 gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
-# Install ttyd from https://github.com/tsl0922/ttyd/releases
-sudo yum install -yq vhs ffmpeg
 
-sudo wget -O /usr/local/bin/ttyd https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64 && chmod +x /usr/local/bin/ttyd
+sudo yum install -yq vhs ffmpeg
+sudo wget --quiet -O $TTYD $TTYD_URL && chmod +x $TTYD
