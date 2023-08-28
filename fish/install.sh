51f38645a76c1ecbd098a58f8ad89cd9c74b3b15
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-08-29 02:28:38 +0400

    fish: added scripts sourcing from my python repo

--- fish/install.sh
+++ fish/install.sh
@@ -1,1 +1,10 @@
 $INSTALLER fish
+
+FISH_DIR="$HOME/.config/fish/"
+SCRIPTS_REPO="https://raw.githubusercontent.com/atimofeev/learning-python/main/"
+
+echo "Sourcing scripts..."
+wget --quiet -O $FISH_DIR/ps_colors.py  $SCRIPTS_REPO/projects/ps_colors.py
+wget --quiet -O $FISH_DIR/prompt_gen.py  $SCRIPTS_REPO/chatgpt/prompt_gen.py
+
+chmod +x $FISH_DIR/*.py
