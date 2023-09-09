89922ff28377e77c470ad8207e8ada96611f6538
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-09-09 23:33:55 +0400

    +mpv: default audio & subs selection

---
+++ mpv/files/scripts/select_subtitle.lua
@@ -0,0 +1,14 @@
+-- Select 'Full' subs by default
+function on_start(_, _)
+    local tracks = mp.get_property_native("track-list")
+
+    for _, track in pairs(tracks) do
+        if track["type"] == "sub" and string.match(track["title"], "Full") then
+            mp.set_property("sid", track["id"])
+            return
+        end
+    end
+end
+
+mp.register_event("file-loaded", on_start)
+
