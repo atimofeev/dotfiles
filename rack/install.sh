837500763c6c8088e669738416efafc967d89100
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-09-12 17:31:39 +0400

    rack: installer to fetch latest version

--- rack/install.sh
+++ rack/install.sh
@@ -1,11 +1,15 @@
-URL="https://vcvrack.com/downloads/RackFree-2.4.0-lin-x64.zip"
-FILENAME=$(basename $URL)
-TMP_DIR=/tmp
+HOME_PAGE="https://vcvrack.com"
+INSTALLER_URL=$(curl --silent --show-error "$HOME_PAGE/Rack" | \
+grep --only-matching '<a class="button rack-button" href="/downloads/RackFree-[^"]*-lin-x64.zip">Linux x64</a>' | \
+grep --only-matching 'href="[^"]*' | cut -d '"' -f2)
+FULL_URL="$HOME_PAGE$INSTALLER_URL"
 
+FILENAME=$(basename $FULL_URL)
+TMP_DIR=/tmp
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-wget --quiet -O $TMP_DIR/$FILENAME $URL
-unzip -q $TMP_DIR/$FILENAME -d $HOME/
+wget --quiet --output-document $TMP_DIR/$FILENAME $FULL_URL
+unzip -o -q $TMP_DIR/$FILENAME -d $HOME/
 rm $TMP_DIR/$FILENAME
 
 git clone git@github.com:atimofeev/vcv-rack2-settings.git $HOME/repos/vcv-rack2-settings/
