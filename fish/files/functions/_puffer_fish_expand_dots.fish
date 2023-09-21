ac88007cd3b001f1cb10d8d19c8d0ef83ca192f2
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-09-21 19:43:37 +0400

    fish: puffer-fish: 'cd. -> cd ..' with custom command list

--- fish/files/functions/_puffer_fish_expand_dots.fish
+++ fish/files/functions/_puffer_fish_expand_dots.fish
@@ -1,6 +1,15 @@
 function _puffer_fish_expand_dots -d 'expand ... to ../.. etc'
     set -l cmd (commandline --cut-at-cursor)
     set -l split (string split ' ' $cmd)
+
+    # Probably needs proper implementation with a dedicated global var and a repo fork
+    # Example: cd. -> cd ..
+    set -l catch_commands cd cp mv
+    if contains $cmd $catch_commands
+        commandline --replace "$cmd .."
+        return 0
+    end
+
     switch $split[-1]
         case './*'; commandline --insert '.'
         case '*..'
