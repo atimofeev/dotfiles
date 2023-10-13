2ee2dbedbdb2b07d7f9300f30716026f360cb182
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-10-13 17:24:34 +0400

    fish: dependency update

--- fish/install.sh
+++ fish/install.sh
@@ -6,6 +6,6 @@ SCRIPTS_REPO="https://raw.githubusercontent.com/atimofeev/learning-python/main/"
 echo "Sourcing scripts..."
 mkdir --parents $HOME_BIN
 wget --quiet --output-document $HOME_BIN/ps_colors.py  $SCRIPTS_REPO/projects/ps_colors.py
-wget --quiet --output-document $HOME_BIN/pclip  $SCRIPTS_REPO/chatgpt/pclip
+wget --quiet --output-document $HOME_BIN/pclip  $SCRIPTS_REPO/projects/chatgpt/pclip
 
 chmod +x $HOME_BIN/*.py $HOME_BIN/pclip
