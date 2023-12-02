2d97760eaecab383b20df02c3a7cb6f9e7c5c6f2
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-12-02 16:14:09 +0400

    fonts: install script fix

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,12 +3,14 @@ FILENAME=$(basename $URL)
 TMP_DIR=/tmp
 FONT_DIR="$HOME/.local/share/fonts"
 
+mkdir -p $FONT_DIR
+
 wget --quiet --output-document $TMP_DIR/$FILENAME $URL
 
 tar -Jxf $TMP_DIR/$FILENAME --directory $FONT_DIR
 
 rm $TMP_DIR/$FILENAME
 
-fc-cache -fv
+fc-cache -fv | tail -1
 
 fc-list : family | grep Nerd
