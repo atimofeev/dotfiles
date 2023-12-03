9657ec2d582452ac245e25a6466a876c89af1d4d
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-12-04 01:05:35 +0400

    fish: py scripts deps path update

--- fish/install.sh
+++ fish/install.sh
@@ -3,11 +3,11 @@ $INSTALLER fish
 chsh --shell /usr/bin/fish
 
 HOME_BIN="$HOME/.bin"
-SCRIPTS_REPO="https://raw.githubusercontent.com/atimofeev/learning-python/main/"
+SCRIPTS_REPO="https://raw.githubusercontent.com/atimofeev/learning/main/"
 
 echo "Sourcing scripts..."
 mkdir --parents $HOME_BIN
-wget --quiet --output-document $HOME_BIN/ps_colors.py  $SCRIPTS_REPO/projects/ps_colors.py
-wget --quiet --output-document $HOME_BIN/pclip  $SCRIPTS_REPO/projects/chatgpt/pclip
+wget --quiet --output-document $HOME_BIN/ps_colors.py  $SCRIPTS_REPO/python/projects/ps_colors.py
+wget --quiet --output-document $HOME_BIN/pclip  $SCRIPTS_REPO/python/projects/chatgpt/pclip
 
 chmod +x $HOME_BIN/*.py $HOME_BIN/pclip
