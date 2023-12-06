2c49c3d45362e79ceb5244aa2378c3824ce23f7d
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-12-06 19:34:59 +0400

    fonts: automatically install latest font version

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,15 +1,24 @@
-URL=https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/DejaVuSansMono.tar.xz
-FILENAME=$(basename $URL)
-TMP_DIR=/tmp
+FONT_FILE="DejaVuSansMono.tar.xz"
+
+LATEST_RELEASE_URL="https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest"
+RELEASE_DATA=$(curl --silent $LATEST_RELEASE_URL)
+DOWNLOAD_URL=$(echo $RELEASE_DATA | jq --raw-output ".assets[] | select(.name == \"$FONT_FILE\") | .browser_download_url")
+
+TMP_DIR="/tmp"
 FONT_DIR="$HOME/.local/share/fonts"
+
+if [ -z "$DOWNLOAD_URL" ]; then
+    echo "Error: Font not found in the latest release."
+    exit 1
+fi
 
 mkdir --parents $FONT_DIR
 
-wget --quiet --output-document $TMP_DIR/$FILENAME $URL
+wget --quiet --output-document $TMP_DIR/$FONT_FILE $DOWNLOAD_URL
 
-tar -Jxf $TMP_DIR/$FILENAME --directory $FONT_DIR
+tar -Jxf $TMP_DIR/$FONT_FILE --directory $FONT_DIR
 
-rm $TMP_DIR/$FILENAME
+rm $TMP_DIR/$FONT_FILE
 
 fc-cache -fv | tail -1
 
