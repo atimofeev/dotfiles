9c1d3f44546b3328a53a705c1ecd27b645ac35ac
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-12-21 20:14:54 +0400

    +etc: gnome setup draft

---
+++ etc/gnome-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Hotkeys
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"  # Free the bind for mc in emacs
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['']"  # Free the bind for mc in emacs
