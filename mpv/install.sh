b23549baa231ff199f1735497ecb3137c9457db8
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-12-23 15:22:05 +0400

    mpv: +mpv-mpris for media keys control

--- mpv/install.sh
+++ mpv/install.sh
@@ -1,1 +1,7 @@
-$INSTALLER mpv
+$INSTALLER mpv playerctl
+
+# Hack to fix MPRIS plugin compiled from older lib versions
+for file in "$(pwd)"/files/scripts/mpris-libs/*; do
+    filename=$(basename "$file")
+    sudo ln -s "$file" /usr/lib64/"$filename"
+done
