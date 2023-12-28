b78be692f5a7e640596cf4039fa9a37d12d9d73d
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-12-28 18:51:35 +0400

    etc: gnome extensions stub

--- etc/gnome-setup.sh
+++ etc/gnome-setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # Hotkeys
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"  # Ctrl-Alt-Up: Free the bind for mc in emacs
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['']"  # Ctrl-Alt-Down: Free the bind for mc in emacs
 
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"  # Free the bind for mc in emacs
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['']"  # Free the bind for mc in emacs
+# Extensions
+https://extensions.gnome.org/extension/4691/pip-on-top/
+sudo dnf install gnome-shell-extension-freon lm_sensors
