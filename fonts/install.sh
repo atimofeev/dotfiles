51d6e5658850a679650a41d8aed15b08cdbb67d6
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2023-12-28 18:49:40 +0400

    fonts: install.sh refactor

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,24 +1,27 @@
-FONT_FILE="DejaVuSansMono.tar.xz"
+REPO_NAME="ryanoasis/nerd-fonts"
+RELEASE_TAG="latest"
 
-LATEST_RELEASE_URL="https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest"
+FILE="DejaVuSansMono.tar.xz"
+
+LATEST_RELEASE_URL="https://api.github.com/repos/$REPO_NAME/releases/$RELEASE_TAG"
 RELEASE_DATA=$(curl --silent $LATEST_RELEASE_URL)
-DOWNLOAD_URL=$(echo $RELEASE_DATA | jq --raw-output ".assets[] | select(.name == \"$FONT_FILE\") | .browser_download_url")
+DOWNLOAD_URL=$(echo $RELEASE_DATA | jq --raw-output ".assets[] | select(.name == \"$FILE\") | .browser_download_url")
 
 TMP_DIR="/tmp"
-FONT_DIR="$HOME/.local/share/fonts"
+DEST_DIR="$HOME/.local/share/fonts"
 
 if [ -z "$DOWNLOAD_URL" ]; then
-    echo "Error: Font not found in the latest release."
+    echo "Error: File $FILE not found in the $RELEASE_TAG release."
     exit 1
 fi
 
-mkdir --parents $FONT_DIR
+mkdir --parents $DEST_DIR
 
-wget --quiet --output-document $TMP_DIR/$FONT_FILE $DOWNLOAD_URL
+wget --quiet --output-document $TMP_DIR/$FILE $DOWNLOAD_URL
 
-tar -Jxf $TMP_DIR/$FONT_FILE --directory $FONT_DIR
+tar -Jxf $TMP_DIR/$FILE --directory $DEST_DIR
 
-rm $TMP_DIR/$FONT_FILE
+rm $TMP_DIR/$FILE
 
 fc-cache -fv | tail -1
 
