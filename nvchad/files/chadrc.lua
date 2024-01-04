554bf654e9127d717ee10747dce3166e34d3ee36
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-01-04 17:49:38 +0400

    nvchad: apply doom emacs theme and colors

--- nvchad/files/chadrc.lua
+++ nvchad/files/chadrc.lua
@@ -2,13 +2,33 @@
 local M = {}
 
 M.ui = {
-  nvdash = {
-    load_on_startup = true,
-  },
+  theme = "doomchad",
+  theme_toggle = { "doomchad", "onedark" },
   hl_override = {
     CursorLine = {
-      bg = "one_bg",
+      bg = "darker_black",
     },
+  },
+  hl_add = {
+    -- Neogit
+    NeogitDiffAdd = { bg = "#3e493d", fg = "green" },
+    NeogitDiffAddHighlight = { bg = "#3e493d", fg = "green" },
+    NeogitDiffDelete = { bg = "#392d34", fg = "red" },
+    NeogitDiffDeleteHighlight = { bg = "#392d34", fg = "red" },
+    NeogitDiffContextHighlight = { bg = "black2" },
+    NeogitHunkHeader = { bg = "lightbg" },
+    NeogitHunkHeaderHighlight = { bg = "grey" },
+    -- RainbowDelimiters
+    RainbowDelimiterRed = { fg = "#51afef" },
+    RainbowDelimiterYellow = { fg = "#c678dd" },
+    RainbowDelimiterBlue = { fg = "#98be65" },
+    RainbowDelimiterOrange = { fg = "#a9a1e1" },
+    RainbowDelimiterGreen = { fg = "#51afef" },
+    RainbowDelimiterViolet = { fg = "#c678dd" },
+    RainbowDelimiterCyan = { fg = "#98be65" },
+  },
+  nvdash = {
+    load_on_startup = true,
   },
 }
 
