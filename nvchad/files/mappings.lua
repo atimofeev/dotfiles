930e3ae82ad92c6d59e0fef3d89776e1fdbb3170
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-01-07 18:29:04 +0400

    nvchad: +SPC-f-r -> Telescope oldfiles

--- nvchad/files/mappings.lua
+++ nvchad/files/mappings.lua
@@ -4,9 +4,10 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>gg"] = { "<cmd> Neogit <CR>", "Open Neogit" },
     ["<M-Up>"] = { "<cmd> m -2 <CR>", "Move line up" },
     ["<M-Down>"] = { "<cmd> m +1 <CR>", "Move line down" },
+    ["<leader>gg"] = { "<cmd> Neogit <CR>", "Open Neogit" },
+    ["<leader>fr"] = { "<cmd> Telescope oldfiles <CR>", "Open Recent Files" },
   },
   v = {
     [">"] = { ">gv", "indent" },
