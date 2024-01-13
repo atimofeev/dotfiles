ce91dfa91c7152ff7a8b2d47303744e0e8bdb96c
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-01-14 01:22:28 +0400

    nvchad: +ts-hyprlang; obsidian.nvim

--- nvchad/files/plugins.lua
+++ nvchad/files/plugins.lua
@@ -7,7 +7,20 @@ local plugins = {
 
   ---- UI/VISIBILITY ----
 
-  { "nvim-treesitter/nvim-treesitter", opts = { auto_install = true } },
+  -- { "nvim-treesitter/nvim-treesitter", opts = { auto_install = true } },
+  {
+    "luckasRanarison/tree-sitter-hyprlang",
+    event = "BufRead",
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+          auto_install = true,
+          ignore_install = { "markdown" },
+        },
+      },
+    },
+  },
 
   { "NvChad/nvcommunity", { import = "nvcommunity.editor.rainbowdelimiters" } },
 
@@ -61,7 +74,7 @@ local plugins = {
     end,
   },
 
-  ---- MISC ----
+  ---- MISC ---
 
   { -- exit INSERT mode with fast "jk"
     "max397574/better-escape.nvim",
@@ -90,6 +103,34 @@ local plugins = {
       }
       require("telescope").load_extension "projects"
     end,
+  },
+
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/repos/vaults/personal/",
+        },
+        {
+          name = "work",
+          path = "~/repos/vaults/work/",
+        },
+      },
+    },
   },
 }
 
